5c690b13aa5d8fc5f72501699c55cdd71aa28e66
Yaruto <neo960211@gmail.com>
2023-06-22 00:15:50 +0200

    Added Pokegame component with random set of card for each Pokedex

--- pokedex-project/src/App.js
+++ pokedex-project/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 
 function App() {
   return (
     <div className='container'>
-      <Pokedex/>
+      <Pokegame/>
     </div>
   );
 }
