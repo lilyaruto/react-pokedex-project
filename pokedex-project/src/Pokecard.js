e30ad044e20bd80caf30b7ea6f6c983c4d51cc4c
Yaruto <neo960211@gmail.com>
2023-06-21 23:43:37 +0200

    Changed images API, added function to control img-id regex

--- pokedex-project/src/Pokecard.js
+++ pokedex-project/src/Pokecard.js
@@ -1,11 +1,20 @@
 import { Component } from "react";
 import './Pokecard.css';
 
-const IMAGE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+const IMAGE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
+
+let formatImgId = (id) => {
+    let strIdLength = (id+"").length;
+    if (strIdLength < 3) {
+        return `${"0".repeat(3 - strIdLength)}${id}`;
+    } else {
+        return id;
+    }
+};
 
 class Pokecard extends Component {
     render() {
-        let imgSrc = `${IMAGE_API}${this.props.id}.png`;
+        let imgSrc = `${IMAGE_API}${formatImgId(this.props.id)}.png`;
         return(
             <div className="card">
                 <span className="pokemon-name">{this.props.name}</span>
